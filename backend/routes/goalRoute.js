e082ccc225e77b2128d8b778cbeeb2ce7973570e
salnish <103054536+salnish@users.noreply.github.com>
2022-08-22 13:28:18 +0530

    stage1

---
+++ backend/routes/goalRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+const goalRouter = require("./routes/goalRoute");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", goalRouter);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
